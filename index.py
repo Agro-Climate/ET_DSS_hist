1c2ed1c567a142e43a72df38a01c923e20a36f42
kewa <kewa@iri.columbia.edu>
2021-07-07 01:19:48 +0100

    ...

--- index.py
+++ index.py
@@ -10,19 +10,25 @@ from app import server
 
 
 from navbar import navbar
-from apps import about
+from apps import about, tutorial, feedback
 from apps.ethiopia import historical as et_hist
 
 
 # Preparing to use a variable for the country
 country = "ethiopia"
 
-apps = {
-    "ethiopia": { "/about": about.layout, "/historical": et_hist.layout, },
-    # "senegal": { "/about": about.layout, "/historical": sn_hist.layout, },
-    # "colombia": { "/about": about.layout, "/historical": co_hist.layout, },
-}
+base_apps = { "/about": about.layout, "/tutorial": tutorial.layout, "/feedback": feedback.layout, }
 
+et_apps = base_apps.update({ "/historical": et_hist.layout, })
+# sn_apps = base_apps.update({ "/historical": sn_hist.layout, })
+# co_apps = base_apps.update({ "/historical": co_hist.layout, })
+
+
+apps = {
+    "ethiopia": { "/about": about.layout, "/tutorial": tutorial.layout, "/feedback": feedback.layout,  "/historical": et_hist.layout, },
+    # "senegal": { "/about": about.layout, "/tutorial": tutorial.layout, "/feedback": feedback.layout,  "/historical": sn_hist.layout, },
+    # "colombia": { "/about": about.layout, "/tutorial": tutorial.layout, "/feedback": feedback.layout,  "/historical": co_hist.layout, },
+}
 
 SIMAGRI_LOGOS = app.get_asset_url("ethioagroclimate.png")
 
