4529633cd875c4bcf77ef60f3be0a50549a7888e
kewa <kewa@iri.columbia.edu>
2021-07-19 23:27:45 +0100

    senegal style mobile

--- apps/senegal/historical.py
+++ apps/senegal/historical.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 import pathlib
 import re
 
-
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
@@ -27,10 +26,6 @@ import bisect   # an element into sorted list
 
 import graph
 
-# sce_col_names=[ "sce_name", "Crop", "Cultivar","stn_name", "Plt-date", "FirstYear", "LastYear", "soil","iH2O","iNO3","TargetYr",
-#                 "Fert_1_DOY","Fert_1_Kg","Fert_2_DOY","Fert_2_Kg","Fert_3_DOY","Fert_3_Kg","Fert_4_DOY","Fert_4_Kg",
-#                 "CropPrice", "NFertCost", "SeedCost","OtherVariableCosts","FixedCosts"
-# ]
 sce_col_names=[ "sce_name", "Crop", "Cultivar","stn_name", "Plt-date", "FirstYear", "LastYear", "soil","iH2O","iNO3","TargetYr",
                 "Fert_1_DOY","N_1_Kg","P_1_Kg","K_1_Kg", "Fert_2_DOY","N_2_Kg","P_2_Kg","K_2_Kg","Fert_3_DOY","N_3_Kg","P_3_Kg","K_3_Kg",
                 "Fert_4_DOY","N_4_Kg","P_4_Kg","K_4_Kg","P_level","IR_1_DOY", "IR_1_amt","IR_2_DOY", "IR_2_amt","IR_3_DOY","IR_3_amt", 
@@ -948,7 +943,7 @@ layout = html.Div([
                         dbc.Col(
                           dbc.Button(id="btn_csv_yield", 
                           children="Simulated Yield", 
-                          className="w-100 d-block mx-auto",
+                          className="d-block mx-auto",
                           color="secondary",
                           ),
                         xs=4,
@@ -957,7 +952,7 @@ layout = html.Div([
                         dbc.Col(
                           dbc.Button(id="btn_csv_Pexe", 
                           children="Prob. of Exceedance", 
-                          className="w-100 d-block mx-auto",
+                          className="d-block mx-auto",
                           color="secondary",
                           ),
                         xs=4,
@@ -966,7 +961,7 @@ layout = html.Div([
                         dbc.Col(
                           dbc.Button(id="btn_csv_rain", 
                           children="Seasonal Rainfall", 
-                          className="w-100 d-block mx-auto",
+                          className="d-block mx-auto",
                           color="secondary",
                           ),
                         xs=4,
