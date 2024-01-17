9f7787e72db0f13287a2e3484e1e2678d0b81ee0
Simagri Server <simagri@simagri.iri.columbia.edu>
2024-01-17 15:56:35 +0000

    renamed SIMAGRI_senega_AICCRA_logo.GIF to SIMAGRI_senega_AICCRA_logo.gif

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ apps = {
     },
     "senegal":  { 
         # "logo": app.get_asset_url("CWP_IRI_ISRA_senegal.GIF"), #IRI_ISRA_senegal.gif"),
-        "logo": app.get_asset_url("SIMAGRI_senegal_AICCRA_logo.GIF"), 
+        "logo": app.get_asset_url("SIMAGRI_senegal_AICCRA_logo.gif"), 
         # "tutorial": "https://sites.google.com/iri.columbia.edu/simagri-senegal/simagri-tutorial",
         "tutorial": "https://sites.google.com/iri.columbia.edu/simagri-french/simagri-tutorial",
         "feedback": "https://sites.google.com/iri.columbia.edu/simagri-senegal/user-feedback-survey-form",
